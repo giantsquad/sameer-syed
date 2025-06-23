fb2ac379e217127317aa356ce0e634e0af954447
Sameer Syed <reachmeatsameer@gmail.com>
2025-06-22 21:46:35 -0400

    Add sitemap.xml/robots.txt, and try fixing sameer syed

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -5,11 +5,10 @@ export default function Home() {
     <div className="grid grid-rows-[20px_1fr_20px] items-center justify-items-center min-h-screen p-8 pb-20 gap-16 sm:p-20 font-[family-name:var(--font-geist-sans)]">
       <main className="flex flex-col gap-[32px] row-start-2 items-center sm:items-start">
         <Image
-          className="dark:invert"
           src="/sameerSyedWebsiteLogos.svg"
           alt="Sameer Syed"
-          width={180}
-          height={38}
+          width={360}
+          height={76}
           priority
         />
         <ol className="list-inside list-decimal text-sm/6 text-center sm:text-left font-[family-name:var(--font-geist-mono)]">
